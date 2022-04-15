c186a19f6298a80b4fb01f77558a6486f46f6f66
plulsls <plusls@qq.com>
2022-04-15 19:31:16 +0800

    add SimpleContainer compat

---
+++ magiclib-compat-mc1_15/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/MixinSimpleContainer.java
@@ -0,0 +1,25 @@
+package top.hendrixshen.magiclib.compat.mixin.minecraft.world;
+
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import top.hendrixshen.magiclib.compat.annotation.Remap;
+
+@Mixin(SimpleContainer.class)
+public abstract class MixinSimpleContainer {
+
+    @Shadow
+    public abstract ItemStack addItem(ItemStack itemStack);
+
+    @Remap("method_7659")
+    public void fromTag(ListTag listTag) {
+        for (int i = 0; i < listTag.size(); ++i) {
+            ItemStack itemStack = ItemStack.of(listTag.getCompound(i));
+            if (!itemStack.isEmpty()) {
+                this.addItem(itemStack);
+            }
+        }
+    }
+}
