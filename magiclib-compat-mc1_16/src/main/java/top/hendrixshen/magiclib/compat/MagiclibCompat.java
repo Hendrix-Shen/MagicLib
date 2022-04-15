f271be56fdd8d2c9b656ee55d2dce7e3d6728b0f
plulsls <plusls@qq.com>
2022-04-15 16:55:12 +0800

    remove modmenu:clientsideOnly check modmenu version in 1.16

---
+++ magiclib-compat-mc1_16/src/main/java/top/hendrixshen/magiclib/compat/MagiclibCompat.java
@@ -0,0 +1,13 @@
+package top.hendrixshen.magiclib.compat;
+
+import net.fabricmc.api.ClientModInitializer;
+import top.hendrixshen.magiclib.dependency.annotation.Dependencies;
+import top.hendrixshen.magiclib.dependency.annotation.Dependency;
+
+public class MagiclibCompat implements ClientModInitializer {
+
+    @Dependencies(and = @Dependency(value = "modmenu", versionPredicate = ">=1.16.22", optional = true))
+    @Override
+    public void onInitializeClient() {
+    }
+}
