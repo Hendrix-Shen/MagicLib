7adfd033f5d4924afd460b05ddd54167017e0694
plulsls <plusls@qq.com>
2022-04-15 15:30:37 +0800

    add levelCompat api

---
+++ magiclib-compat-mc1_14/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/level/MixinLevel.java
@@ -0,0 +1,22 @@
+package top.hendrixshen.magiclib.compat.mixin.minecraft.world.level;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.Dimension;
+import net.minecraft.world.level.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import top.hendrixshen.magiclib.compat.world.level.LevelCompatApi;
+
+@Mixin(Level.class)
+public class MixinLevel implements LevelCompatApi {
+    @Shadow
+    @Final
+    public Dimension dimension;
+
+    @Override
+    public ResourceLocation getDimensionLocation() {
+        return DimensionType.getName(this.dimension.getType());
+    }
+}
---
+++ magiclib-core/src/main/java/top/hendrixshen/magiclib/compat/world/level/LevelCompatApi.java
@@ -0,0 +1,7 @@
+package top.hendrixshen.magiclib.compat.world.level;
+
+import net.minecraft.resources.ResourceLocation;
+
+public interface LevelCompatApi {
+    ResourceLocation getDimensionLocation();
+}
