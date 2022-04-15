4cf53893b129200207cc3f794fcebc0e4447dd27
plulsls <plusls@qq.com>
2022-04-15 19:50:31 +0800

    add Entity.getLevel

--- magiclib-compat-mc1_14/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/entity/MixinEntity.java
+++ magiclib-compat-mc1_14/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/entity/MixinEntity.java
@@ -3,6 +3,7 @@ package top.hendrixshen.magiclib.compat.mixin.minecraft.world.entity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import top.hendrixshen.magiclib.compat.annotation.Remap;
@@ -17,6 +18,9 @@ public abstract class MixinEntity {
 
     @Shadow
     public abstract BlockPos getCommandSenderBlockPosition();
+
+    @Shadow
+    public Level level;
 
     @Remap("method_9203")
     public void sendMessage(Component component, UUID uuid) {
@@ -41,5 +45,10 @@ public abstract class MixinEntity {
     @Remap("method_31479")
     public int getBlockZ() {
         return this.blockPosition().getZ();
+    }
+
+    @Remap("method_37908")
+    public Level getLevel() {
+        return this.level;
     }
 }
--- magiclib-compat-mc1_17/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/entity/MixinEntity.java
+++ magiclib-compat-mc1_17/src/main/java/top/hendrixshen/magiclib/compat/mixin/minecraft/world/entity/MixinEntity.java
@@ -1,6 +1,5 @@
 package top.hendrixshen.magiclib.compat.mixin.minecraft.world.entity;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,25 +9,7 @@ import top.hendrixshen.magiclib.compat.annotation.Remap;
 @Mixin(Entity.class)
 public class MixinEntity {
     @Shadow
-    private BlockPos blockPosition;
-
-    @Shadow
     public Level level;
-
-    @Remap("method_31477")
-    public int getBlockX() {
-        return this.blockPosition.getX();
-    }
-
-    @Remap("method_31478")
-    public int getBlockY() {
-        return this.blockPosition.getY();
-    }
-
-    @Remap("method_31479")
-    public int getBlockZ() {
-        return this.blockPosition.getZ();
-    }
 
     @Remap("method_37908")
     public Level getLevel() {
