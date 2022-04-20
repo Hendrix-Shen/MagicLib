bdcd980c8aac4f59d1c0335a650b1b1e6001f41a
plusls <plusls@qq.com>
2022-04-20 23:17:05 +0800

    fix bug in VertexConsumerCompat

--- magiclib-compat-mc1_14/src/main/java/com/mojang/blaze3d/vertex/VertexConsumerCompat.java
+++ magiclib-compat-mc1_14/src/main/java/com/mojang/blaze3d/vertex/VertexConsumerCompat.java
@@ -5,11 +5,13 @@ import top.hendrixshen.magiclib.compat.annotation.Remap;
 @Remap("net/minecraft/class_4588")
 public interface VertexConsumerCompat {
 
-    @Remap("method_1336")
-    VertexConsumerCompat color(int i, int j, int k, int l);
-
     @Remap("method_22915")
     default VertexConsumerCompat color(float f, float g, float h, float i) {
-        return this.color((int) (f * 255), (int) (g * 255), (int) (h * 255), (int) (i * 255.0F));
+        return (VertexConsumerCompat) ((BufferBuilder) this).color((int) (f * 255), (int) (g * 255), (int) (h * 255), (int) (i * 255.0F));
+    }
+
+    @Remap("method_1344")
+    default void endVertex() {
+        ((BufferBuilder) this).endVertex();
     }
 }
