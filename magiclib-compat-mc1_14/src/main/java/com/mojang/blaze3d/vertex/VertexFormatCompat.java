361aabc46879e27e0e588f794d9979d8697be3b6
plusls <plusls@qq.com>
2022-04-20 23:17:48 +0800

    use GL11 constant

--- magiclib-compat-mc1_14/src/main/java/com/mojang/blaze3d/vertex/VertexFormatCompat.java
+++ magiclib-compat-mc1_14/src/main/java/com/mojang/blaze3d/vertex/VertexFormatCompat.java
@@ -1,5 +1,6 @@
 package com.mojang.blaze3d.vertex;
 
+import org.lwjgl.opengl.GL11;
 import top.hendrixshen.magiclib.compat.annotation.InnerClass;
 import top.hendrixshen.magiclib.compat.annotation.Remap;
 
@@ -8,7 +9,7 @@ public class VertexFormatCompat {
     @Remap("net/minecraft/class_293$class_5596")
     public static class Mode {
         @Remap("field_27382")
-        public static final Mode QUADS = new Mode(7);
+        public static final Mode QUADS = new Mode(GL11.GL_QUADS);
         public final int value;
 
         public Mode(int value) {
