d316211a8e721b051200766b1cc5512df3c8db4c
plusls <plusls@qq.com>
2022-04-23 19:01:12 +0800

    add ItemStackCompatApi.isSameItemSameTags

--- src/main/java/top/hendrixshen/magiclib/compat/minecraft/world/item/ItemStackCompatApi.java
+++ src/main/java/top/hendrixshen/magiclib/compat/minecraft/world/item/ItemStackCompatApi.java
@@ -4,10 +4,19 @@ package top.hendrixshen.magiclib.compat.minecraft.world.item;
 //$$ import net.minecraft.world.item.Item;
 //#endif
 
+import net.minecraft.world.item.ItemStack;
+
 public interface ItemStackCompatApi {
     //#if MC <= 11605
     //$$ default boolean is(Item item) {
     //$$     throw new UnsupportedOperationException();
     //$$ }
     //#endif
+    static boolean isSameItemSameTags(ItemStack itemStack, ItemStack itemStack2) {
+        //#if MC > 11605
+        return ItemStack.isSameItemSameTags(itemStack, itemStack2);
+        //#else
+        //$$ return itemStack.is(itemStack2.getItem()) && ItemStack.tagMatches(itemStack, itemStack2);
+        //#endif
+    }
 }
