f7b4922ebf74bf69ff8d89bbf96018adbbdc7785
plulsls <plusls@qq.com>
2022-04-25 20:51:04 +0800

    bump version to 0.5

--- magiclib-compat-mc1_14/src/main/java/net/minecraft/client/renderer/ShaderInstanceCompat.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.client.renderer;
-
-import top.hendrixshen.magiclib.compat.annotation.Remap;
-
-@Remap("net/minecraft/class_5944")
-public class ShaderInstanceCompat {
-}
--- magiclib-compat-mc1_14/src/main/java/net/minecraft/network/chat/FormattedTextCompat.java
+++
@@ -1,7 +0,0 @@
-package net.minecraft.network.chat;
-
-import top.hendrixshen.magiclib.compat.annotation.Remap;
-
-@Remap("net/minecraft/class_5348")
-public interface FormattedTextCompat {
-}
