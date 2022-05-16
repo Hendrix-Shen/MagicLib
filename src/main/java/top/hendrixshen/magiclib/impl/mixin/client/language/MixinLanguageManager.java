441e9252e785be4a189d3f18f025c08b8b0e1007
plusls <plusls@qq.com>
2022-05-16 20:03:28 +0800

    fix MixinLanguageManager crash in <= 1.15

--- src/main/java/top/hendrixshen/magiclib/impl/mixin/client/language/MixinLanguageManager.java
+++ src/main/java/top/hendrixshen/magiclib/impl/mixin/client/language/MixinLanguageManager.java
@@ -22,12 +22,14 @@ public class MixinLanguageManager {
     @Shadow
     private String currentCode;
 
+    //#if MC > 11502
     @Shadow
     private Map<String, LanguageInfo> languages;
 
     @Shadow
     @Final
     private static LanguageInfo DEFAULT_LANGUAGE;
+    //#endif
 
     @Inject(method = "<init>", at = @At(value = "RETURN"))
     private void postInit(String string, CallbackInfo ci) {
@@ -39,6 +41,7 @@ public class MixinLanguageManager {
         MagicLanguageManager.INSTANCE.setCurrentCode(this.currentCode);
     }
 
+    //#if MC > 11502
     @ModifyVariable(method = "onResourceManagerReload", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/client/resources/language/ClientLanguage;loadFrom(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/List;)Lnet/minecraft/client/resources/language/ClientLanguage;",
             ordinal = 0))
@@ -66,4 +69,19 @@ public class MixinLanguageManager {
 
         return languageInfoList;
     }
+    //#else
+    //$$ @ModifyVariable(method = "onResourceManagerReload", at = @At(value = "INVOKE",
+    //$$         target = "Lnet/minecraft/client/resources/language/Locale;loadFrom(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/List;)V",
+    //$$         ordinal = 0))
+    //$$ private List<String> addFallbackLanguage(List<String> languageCodeList) {
+    //$$     ArrayList<String> codes = new ArrayList<>(MagicLibConfigs.fallbackLanguageList);
+    //$$     if (!codes.contains(currentCode)) {
+    //$$         codes.add(0, currentCode);
+    //$$     }
+    //$$     if (!codes.contains(MagicLanguageManager.DEFAULT_CODE)) {
+    //$$         codes.add(MagicLanguageManager.DEFAULT_CODE);
+    //$$     }
+    //$$     return codes;
+    //$$ }
+    //#endif
 }
