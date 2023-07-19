6e93de3a2be422d2ab3d3a4bc0692d7a16ccd87e
Hendrix-Shen <HendrixShen@hendrixshen.top>
2023-07-19 20:20:08 +0800

    Fix missing impl
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- src/main/java/top/hendrixshen/magiclib/malilib/impl/config/TranslatableConfigString.java
+++ src/main/java/top/hendrixshen/magiclib/malilib/impl/config/TranslatableConfigString.java
@@ -1,8 +1,13 @@
 package top.hendrixshen.magiclib.malilib.impl.config;
 
+import com.google.gson.JsonElement;
+import fi.dy.masa.malilib.config.options.ConfigBase;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
 
 @Deprecated()
 @ApiStatus.ScheduledForRemoval(inVersion = "0.8")
@@ -10,5 +15,35 @@ import org.jetbrains.annotations.ApiStatus;
 public class TranslatableConfigString extends MagicConfigString {
     public TranslatableConfigString(String prefix, String name, String defaultValue) {
         super(prefix, name, defaultValue);
+    }
+
+    @Override
+    public void setValueFromJsonElement(JsonElement jsonElement) {
+        super.setValueFromJsonElement(jsonElement);
+    }
+
+    @Override
+    public @Nullable Consumer<ConfigBase<?>> getValueChangedFromJsonCallback() {
+        return super.getValueChangedFromJsonCallback();
+    }
+
+    @Override
+    public void setValueChangedFromJsonCallback(@Nullable Consumer<ConfigBase<?>> valueChangedFromJsonCallback) {
+        super.setValueChangedFromJsonCallback(valueChangedFromJsonCallback);
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getPrettyName() {
+        return super.getPrettyName();
+    }
+
+    @Override
+    public String getComment() {
+        return super.getComment();
     }
 }
