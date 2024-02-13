4a3b6f353af47b31d0fd6a2e013279a17021c13a
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-02-13 12:20:03 +0800

    Add basic Forge & NeoForge support
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

---
+++ magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/entrypoint/minecraft/MagicLibForge.java
@@ -0,0 +1,9 @@
+//#if FORGE
+//$$ package top.hendrixshen.magiclib.entrypoint.minecraft;
+//$$
+//$$ import net.minecraftforge.fml.common.Mod;
+//$$
+//$$ @Mod("magiclib_minecraft_api")
+//$$ public class MagicLibForge {
+//$$ }
+//#endif
---
+++ magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/entrypoint/minecraft/MagicLibNeoForge.java
@@ -0,0 +1,9 @@
+//#if NEO_FORGE
+//$$ package top.hendrixshen.magiclib.entrypoint.minecraft;
+//$$
+//$$ import net.neoforged.fml.common.Mod;
+//$$
+//$$ @Mod("magiclib_minecraft_api")
+//$$ public class MagicLibNeoForge {
+//$$ }
+//#endif
