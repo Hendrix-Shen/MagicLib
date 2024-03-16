08b8c911140732b7bc514456d0651bed3800bf51
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-03-16 17:30:29 +0800

    Fix jikuTsuiho
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-core/common/src/main/java/top/hendrixshen/magiclib/impl/mixin/extension/jikuTsuiho/EraserExtension.java
+++ magiclib-core/common/src/main/java/top/hendrixshen/magiclib/impl/mixin/extension/jikuTsuiho/EraserExtension.java
@@ -1,6 +1,8 @@
 package top.hendrixshen.magiclib.impl.mixin.extension.jikuTsuiho;
 
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import org.spongepowered.asm.mixin.transformer.ext.ITargetClassContext;
@@ -13,6 +15,7 @@ import top.hendrixshen.magiclib.impl.mixin.extension.jikuTsuiho.info.MixinFieldI
 import top.hendrixshen.magiclib.impl.mixin.extension.jikuTsuiho.info.MixinMethodInfoImpl;
 import top.hendrixshen.magiclib.util.mixin.MixinInternals;
 
+import java.util.Iterator;
 import java.util.SortedSet;
 
 public class EraserExtension extends EmptyExtension {
@@ -24,54 +27,67 @@ public class EraserExtension extends EmptyExtension {
     @Override
     public void preApply(ITargetClassContext context) {
         SortedSet<IMixinInfo> mixins = MixinInternals.getMixins(context);
+        Iterator<IMixinInfo> mixinInfoIterator = mixins.iterator();
 
-        mixins.removeIf(info -> {
+        while (mixinInfoIterator.hasNext()) {
+            IMixinInfo info = mixinInfoIterator.next();
             MixinClassInfoImpl mixinClassInfo = new MixinClassInfoImpl(info, context.getClassNode());
             boolean tsuihoClass = MixinEraserManager.shouldTsuiho(mixinClassInfo);
 
             if (tsuihoClass) {
+                mixinInfoIterator.remove();
+
                 if (MixinEnvironment.getCurrentEnvironment()
                         .getOption(MixinEnvironment.Option.DEBUG_EXPORT)) {
                     MagicLib.getLogger().warn("JikuTsuiho: {}", info.getClassName());
                 }
-
-                return true;
             }
 
+            boolean tsuiho = false;
             ClassNode classNode = MixinInternals.getStateClassNode(info);
-            classNode.methods.removeIf(methodNode -> {
+            Iterator<MethodNode> methodNodeIterator = classNode.methods.iterator();
+
+            while (methodNodeIterator.hasNext()) {
+                MethodNode methodNode = methodNodeIterator.next();
                 MixinMethodInfo methodInfo = MixinMethodInfoImpl.of(info, methodNode);
                 boolean tsuihoMethod = MixinEraserManager.shouldTsuiho(methodInfo);
 
-                if (tsuihoMethod && MixinEnvironment.getCurrentEnvironment()
-                        .getOption(MixinEnvironment.Option.DEBUG_EXPORT)) {
-                    MagicLib.getLogger().warn("JikuTsuiho: {} {} {}", info.getClassName(),
-                            methodInfo.getName(), methodInfo.getDesc());
+                if (tsuihoMethod) {
+                    methodNodeIterator.remove();
+                    tsuiho = true;
+
+                    if (MixinEnvironment.getCurrentEnvironment()
+                            .getOption(MixinEnvironment.Option.DEBUG_EXPORT)) {
+                        MagicLib.getLogger().warn("JikuTsuiho: {} {} {}", info.getClassName(),
+                                methodInfo.getName(), methodInfo.getDesc());
+                    }
                 }
+            }
 
-                return tsuihoMethod;
-            });
 
-            classNode.fields.removeIf(fieldNode -> {
+            Iterator<FieldNode> fieldNodeIterator = classNode.fields.iterator();
+
+            while (fieldNodeIterator.hasNext()) {
+                FieldNode fieldNode = fieldNodeIterator.next();
                 MixinFieldInfo fieldInfo = new MixinFieldInfoImpl(info, fieldNode);
                 boolean tsuihoField = MixinEraserManager.shouldTsuiho(fieldInfo);
 
-                if (tsuihoField && MixinEnvironment.getCurrentEnvironment()
-                        .getOption(MixinEnvironment.Option.DEBUG_EXPORT)) {
-                    MagicLib.getLogger().warn("JikuTsuiho: {} {} {}", info.getClassName(),
-                            fieldInfo.getName(), fieldInfo.getDesc());
+                if (tsuihoField) {
+                    fieldNodeIterator.remove();
+                    tsuiho = true;
+
+                    if (MixinEnvironment.getCurrentEnvironment()
+                            .getOption(MixinEnvironment.Option.DEBUG_EXPORT)) {
+                        MagicLib.getLogger().warn("JikuTsuiho: {} {} {}", info.getClassName(),
+                                fieldInfo.getName(), fieldInfo.getDesc());
+                    }
                 }
-
-                return tsuihoField;
-            });
-
-            tsuihoClass = classNode.methods.isEmpty() && classNode.fields.isEmpty();
-
-            if (tsuihoClass) {
-                MagicLib.getLogger().warn("JikuTsuiho: {}", info.getClassName());
             }
 
-            return classNode.methods.isEmpty() && classNode.fields.isEmpty();
-        });
+            if (tsuiho && classNode.fields.isEmpty() && classNode.methods.isEmpty()) {
+                mixinInfoIterator.remove();
+                MagicLib.getLogger().warn("JikuTsuiho: {}", info.getClassName());
+            }
+        }
     }
 }
