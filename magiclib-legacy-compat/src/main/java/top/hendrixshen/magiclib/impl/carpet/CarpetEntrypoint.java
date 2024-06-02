ac88dfa8351c70988480e3a68f9aabb9fb2e5561
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-06-02 17:34:42 +0800

    Fix version
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/impl/carpet/CarpetEntrypoint.java
+++ magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/impl/carpet/CarpetEntrypoint.java
@@ -1,19 +1,16 @@
 package top.hendrixshen.magiclib.impl.carpet;
 
-import carpet.settings.ParsedRule;
 import org.jetbrains.annotations.Nullable;
-import top.hendrixshen.magiclib.MagicLibReference;
+import top.hendrixshen.magiclib.SharedConstants;
 import top.hendrixshen.magiclib.carpet.impl.WrappedSettingManager;
 import top.hendrixshen.magiclib.util.FabricUtil;
 import top.hendrixshen.magiclib.util.ReflectionUtil;
-
-import java.util.Arrays;
 
 public class CarpetEntrypoint {
     private static final boolean isCarpetLoaded = FabricUtil.isModLoaded("carpet");
 
     public static @Nullable WrappedSettingManager getSettingManager() {
-        return isCarpetLoaded ? WrappedSettingManager.get(MagicLibReference.getModIdentifier()) : null;
+        return isCarpetLoaded ? WrappedSettingManager.get(SharedConstants.getMagiclibIdentifier()) : null;
     }
 
     public static void init() {
@@ -29,10 +26,10 @@ public class CarpetEntrypoint {
                         ReflectionUtil.getClass("top.hendrixshen.magiclib.carpet.api.CarpetExtensionCompatApi")
                                 .orElseThrow(RuntimeException::new)
                 },
-                MagicLibReference.getModIdentifier(),
-                new MagicLibSettingManager(MagicLibReference.getModVersion(),
-                        MagicLibReference.getModIdentifier(),
-                        MagicLibReference.getModNameCurrent()),
+                SharedConstants.getMagiclibIdentifier(),
+                new MagicLibSettingManager(SharedConstants.getMagiclibIdentifier(),
+                        SharedConstants.getMagiclibIdentifier(),
+                        SharedConstants.getMagiclibName()),
                 ReflectionUtil.newInstance("top.hendrixshen.magiclib.impl.carpet.MagicLibAddition", null)
                         .orElseThrow(RuntimeException::new));
     }
