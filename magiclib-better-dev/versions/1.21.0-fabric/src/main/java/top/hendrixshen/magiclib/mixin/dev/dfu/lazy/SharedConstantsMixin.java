f9fa1c1521a39d1a25ef42ad1c68a7199b989ffb
Cjsah <cjsah@cjsah.net>
2024-06-26 23:52:00 +0800

    成功启动

--- magiclib-better-dev/versions/1.21.0-fabric/src/main/java/top/hendrixshen/magiclib/mixin/dev/dfu/lazy/SharedConstantsMixin.java
+++ magiclib-better-dev/versions/1.21.0-fabric/src/main/java/top/hendrixshen/magiclib/mixin/dev/dfu/lazy/SharedConstantsMixin.java
@@ -1,8 +1,19 @@
 package top.hendrixshen.magiclib.mixin.dev.dfu.lazy;
 
+import com.mojang.datafixers.DSL;
+import net.minecraft.client.main.Main;
+import net.minecraft.util.datafix.DataFixers;
 import org.spongepowered.asm.mixin.Mixin;
-import top.hendrixshen.magiclib.api.preprocess.DummyClass;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(DummyClass.class)
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(Main.class)
 public class SharedConstantsMixin {
+    @Redirect(method = "main", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/datafix/DataFixers;optimize(Ljava/util/Set;)Ljava/util/concurrent/CompletableFuture;"))
+    private static CompletableFuture<?> onOptimize(Set<DSL.TypeReference> set) {
+        return DataFixers.optimize(Set.of());
+    }
 }
