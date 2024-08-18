d0f6092cdc89740cd8c43941edf5e540b8ff100c
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-08-18 18:22:30 +0800

    Add a shortcut to BlockStateCompat
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/api/compat/minecraft/world/level/LevelCompat.java
+++ magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/api/compat/minecraft/world/level/LevelCompat.java
@@ -1,8 +1,11 @@
 package top.hendrixshen.magiclib.api.compat.minecraft.world.level;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import top.hendrixshen.magiclib.api.compat.minecraft.world.level.state.BlockStateCompat;
 import top.hendrixshen.magiclib.impl.compat.minecraft.world.level.LevelCompatImpl;
 import top.hendrixshen.magiclib.util.collect.Provider;
 
@@ -10,6 +13,10 @@ public interface LevelCompat extends Provider<Level> {
     static @NotNull LevelCompat of(Level level) {
         return new LevelCompatImpl(level);
     }
+
+    BlockState getBlockState(BlockPos blockPos);
+
+    BlockStateCompat getBlockStateCompat(BlockPos blockPos);
 
     ResourceLocation getDimensionLocation();
 
--- magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/impl/compat/minecraft/world/level/LevelCompatImpl.java
+++ magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/impl/compat/minecraft/world/level/LevelCompatImpl.java
@@ -1,15 +1,28 @@
 package top.hendrixshen.magiclib.impl.compat.minecraft.world.level;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import top.hendrixshen.magiclib.api.compat.AbstractCompat;
 import top.hendrixshen.magiclib.api.compat.minecraft.world.level.LevelCompat;
+import top.hendrixshen.magiclib.api.compat.minecraft.world.level.state.BlockStateCompat;
 import top.hendrixshen.magiclib.impl.compat.minecraft.world.level.dimension.DimensionWrapper;
 
 public class LevelCompatImpl extends AbstractCompat<Level> implements LevelCompat {
     public LevelCompatImpl(@NotNull Level type) {
         super(type);
+    }
+
+    @Override
+    public BlockState getBlockState(BlockPos blockPos) {
+        return this.get().getBlockState(blockPos);
+    }
+
+    @Override
+    public BlockStateCompat getBlockStateCompat(BlockPos blockPos) {
+        return BlockStateCompat.of(this.getBlockState(blockPos));
     }
 
     @Override
