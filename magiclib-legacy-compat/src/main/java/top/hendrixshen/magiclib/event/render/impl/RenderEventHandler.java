fc10cd057ca500d744216f563bc04078462be45c
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-10-24 09:05:16 +0800

    magiclib-legacy-compat 1.21.2
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/event/render/impl/RenderEventHandler.java
+++ magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/event/render/impl/RenderEventHandler.java
@@ -3,9 +3,9 @@ package top.hendrixshen.magiclib.event.render.impl;
 import com.google.common.collect.Lists;
 import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.Getter;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import top.hendrixshen.magiclib.api.compat.minecraft.util.ProfilerCompat;
 import top.hendrixshen.magiclib.event.render.api.PostRenderEntityEvent;
 import top.hendrixshen.magiclib.event.render.api.PostRenderLevelEvent;
 
@@ -14,7 +14,6 @@ import java.util.List;
 public class RenderEventHandler {
     @Getter
     private static final RenderEventHandler instance = new RenderEventHandler();
-    private static final Minecraft mc = Minecraft.getInstance();
     private static final List<PostRenderEntityEvent> postRenderEntityEvents = Lists.newArrayList();
     private static final List<PostRenderLevelEvent> postRenderLevelEvents = Lists.newArrayList();
 
@@ -28,25 +27,25 @@ public class RenderEventHandler {
 
     public void dispatchPostRenderEntityEvent(Entity entity, PoseStack poseStack, float tickDelta) {
         if (!RenderEventHandler.postRenderEntityEvents.isEmpty()) {
-            mc.getProfiler().push("MagicRenderEventHandler::dispatchPostRenderEntityEvent");
+            ProfilerCompat.get().push("MagicRenderEventHandler::dispatchPostRenderEntityEvent");
             RenderContext renderContext = new RenderContext(poseStack);
             RenderEventHandler.postRenderEntityEvents.forEach(event -> {
-                mc.getProfiler().push(event.getProfilerSectionSupplier());
+                ProfilerCompat.get().push(event.getProfilerSectionSupplier());
                 event.render(entity, renderContext, tickDelta);
-                mc.getProfiler().pop();
+                ProfilerCompat.get().pop();
             });
-            mc.getProfiler().pop();
+            ProfilerCompat.get().pop();
         }
     }
 
     public void dispatchPostRenderLevelEvent(Level level, PoseStack poseStack, float tickDelta) {
         if (!RenderEventHandler.postRenderLevelEvents.isEmpty()) {
-            mc.getProfiler().popPush("MagicRenderEventHandler::dispatchPostRenderLevelEvent");
+            ProfilerCompat.get().popPush("MagicRenderEventHandler::dispatchPostRenderLevelEvent");
             RenderContext renderContext = new RenderContext(poseStack);
             RenderEventHandler.postRenderLevelEvents.forEach(event -> {
-                mc.getProfiler().push(event.getProfilerSectionSupplier());
+                ProfilerCompat.get().push(event.getProfilerSectionSupplier());
                 event.render(level, renderContext, tickDelta);
-                mc.getProfiler().pop();
+                ProfilerCompat.get().pop();
             });
         }
     }
