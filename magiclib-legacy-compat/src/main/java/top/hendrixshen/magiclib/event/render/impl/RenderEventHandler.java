34562f7db0ec68ce8d49b0db003d89f80fe6579a
Hendrix-Shen <HendrixShen@hendrixshen.top>
2024-10-24 21:59:02 +0800

    Fix Mixin
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/event/render/impl/RenderEventHandler.java
+++ magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/event/render/impl/RenderEventHandler.java
@@ -5,15 +5,24 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.Getter;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import top.hendrixshen.magiclib.MagicLib;
 import top.hendrixshen.magiclib.api.compat.minecraft.util.ProfilerCompat;
+import top.hendrixshen.magiclib.api.event.minecraft.render.RenderEntityListener;
+import top.hendrixshen.magiclib.api.event.minecraft.render.RenderLevelListener;
 import top.hendrixshen.magiclib.event.render.api.PostRenderEntityEvent;
 import top.hendrixshen.magiclib.event.render.api.PostRenderLevelEvent;
+import top.hendrixshen.magiclib.util.CommonUtil;
 
 import java.util.List;
 
-public class RenderEventHandler {
+public class RenderEventHandler implements RenderLevelListener, RenderEntityListener {
     @Getter
-    private static final RenderEventHandler instance = new RenderEventHandler();
+    private static final RenderEventHandler instance = CommonUtil.make(() -> {
+        RenderEventHandler handler = new RenderEventHandler();
+        MagicLib.getInstance().getEventManager().register(RenderLevelListener.class, handler);
+        MagicLib.getInstance().getEventManager().register(RenderEntityListener.class, handler);
+        return handler;
+    });
     private static final List<PostRenderEntityEvent> postRenderEntityEvents = Lists.newArrayList();
     private static final List<PostRenderLevelEvent> postRenderLevelEvents = Lists.newArrayList();
 
@@ -39,14 +48,48 @@ public class RenderEventHandler {
     }
 
     public void dispatchPostRenderLevelEvent(Level level, PoseStack poseStack, float tickDelta) {
-        if (!RenderEventHandler.postRenderLevelEvents.isEmpty()) {
-            ProfilerCompat.get().popPush("MagicRenderEventHandler::dispatchPostRenderLevelEvent");
-            RenderContext renderContext = new RenderContext(poseStack);
-            RenderEventHandler.postRenderLevelEvents.forEach(event -> {
-                ProfilerCompat.get().push(event.getProfilerSectionSupplier());
-                event.render(level, renderContext, tickDelta);
-                ProfilerCompat.get().pop();
-            });
-        }
+        // NO-OP
+    }
+
+    @Override
+    public void preRenderEntity(Entity entity, top.hendrixshen.magiclib.api.render.context.RenderContext renderContext, float partialTicks) {
+        // NO-OP
+    }
+
+    @Override
+    public void postRenderEntity(Entity entity, top.hendrixshen.magiclib.api.render.context.RenderContext renderContext, float partialTicks) {
+        RenderContext context = new RenderContext(
+                //#if MC > 11502
+                renderContext.getMatrixStack().getPoseStack()
+                //#else
+                //$$ new PoseStack()
+                //#endif
+        );
+        RenderEventHandler.postRenderEntityEvents.forEach(event -> {
+            ProfilerCompat.get().push(event.getProfilerSectionSupplier());
+            event.render(entity, context, partialTicks);
+            ProfilerCompat.get().pop();
+        });
+    }
+
+    @Override
+    public void preRenderLevel(Level level, top.hendrixshen.magiclib.api.render.context.RenderContext renderContext, float partialTicks) {
+        // NO-OP
+    }
+
+    @Override
+    public void postRenderLevel(Level level, top.hendrixshen.magiclib.api.render.context.RenderContext renderContext, float partialTicks) {
+        RenderContext context = new RenderContext(
+                //#if MC > 11502
+                renderContext.getMatrixStack().getPoseStack()
+                //#else
+                //$$ new PoseStack()
+                //#endif
+        );
+        RenderEventHandler.postRenderLevelEvents.forEach(event -> {
+            ProfilerCompat.get().push(event.getProfilerSectionSupplier());
+            event.render(level, context, partialTicks);
+            ProfilerCompat.get().pop();
+        });
     }
 }
