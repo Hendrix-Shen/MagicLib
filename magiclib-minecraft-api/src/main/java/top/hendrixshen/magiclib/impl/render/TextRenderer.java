Hendrix-Shen/MagicLib

commit 1c5d91f2514a3e1362a4169af43ac0aebf2c395d

Minecraft 1.21.2/3 support (#116)
* magiclib-better-dev 1.21.2

* magiclib-minecraft-api 1.21.2

* magiclib-malilib-extra 1.21.2

* magiclib-legacy-compat 1.21.2

* 1.21.2 -> 1.21.3

* Fix Mixin

---------

Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/impl/render/TextRenderer.java
@@ -158,7 +158,9 @@ public class TextRenderer {
                 this.verticalAlignment.getTranslateY(totalTextHeight), 0);
         context.translate(this.shiftX, this.shiftY, 0);
         //#if MC > 11605
+        //#if MC < 12101
         //$$ RenderSystem.applyModelViewMatrix();
+        //#endif
         //#else
         RenderGlobal.enableAlphaTest();
         //#endif
@@ -224,7 +226,7 @@ public class TextRenderer {
         RenderGlobal.enableDepthTest();
         //#endif
         positionTransformer.restore();
-        //#if MC > 11605
+        //#if MC > 11605 && MC < 12101
         //$$ RenderSystem.applyModelViewMatrix();
         //#endif
     }

magiclib-better-dev/build.gradle
@@ -24,6 +24,7 @@ preprocess {
     Node mc_12004_fabric = createNode("better-dev-1.20.4-fabric", 1_20_04, "mojang")
     Node mc_12006_fabric = createNode("better-dev-1.20.6-fabric", 1_20_06, "mojang")
     Node mc_12101_fabric = createNode("better-dev-1.21.1-fabric", 1_21_01, "mojang")
+    Node mc_12103_fabric = createNode("better-dev-1.21.3-fabric", 1_21_03, "mojang")
 
     mc_11404_fabric.link(mc_11502_fabric, null)
     mc_11502_fabric.link(mc_11605_fabric, file("versions/mapping-fabric-1.15.2-1.16.5.txt"))
@@ -37,6 +38,7 @@ preprocess {
     mc_12002_fabric.link(mc_12004_fabric, null)
     mc_12004_fabric.link(mc_12006_fabric, null)
     mc_12006_fabric.link(mc_12101_fabric, null)
+    mc_12101_fabric.link(mc_12103_fabric, null)
 
     // Forge
     Node mc_11701_forge = createNode("better-dev-1.17.1-forge", 1_17_01, "mojang")
@@ -53,8 +55,10 @@ preprocess {
     Node mc_12002_neoforge = createNode("better-dev-1.20.2-neoforge", 1_20_02, "mojang")
     Node mc_12006_neoforge = createNode("better-dev-1.20.6-neoforge", 1_20_06, "mojang")
     Node mc_12101_neoforge = createNode("better-dev-1.21.1-neoforge", 1_21_01, "mojang")
+    Node mc_12103_neoforge = createNode("better-dev-1.21.3-neoforge", 1_21_03, "mojang")
 
     mc_12002_fabric.link(mc_12002_neoforge, file("versions/mapping-1.20.2-fabric-neoforge.txt"))
     mc_12006_fabric.link(mc_12006_neoforge, file("versions/mapping-1.20.6-fabric-neoforge.txt"))
     mc_12101_fabric.link(mc_12101_neoforge, file("versions/mapping-1.21.1-fabric-neoforge.txt"))
+    mc_12103_fabric.link(mc_12103_neoforge, file("versions/mapping-1.21.3-fabric-neoforge.txt"))
 }

magiclib-better-dev/src/main/java/top/hendrixshen/magiclib/impl/dev/threadtweak/ThreadTweaker.java
@@ -35,6 +35,10 @@ import top.hendrixshen.magiclib.mixin.dev.accessor.UtilAccessor;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+//#if MC > 12101
+//$$ import net.minecraft.TracingExecutor;
+//#endif
+
 //#if MC > 12006
 //$$ import net.minecraft.ReportType;
 //#endif
@@ -53,9 +57,16 @@ public class ThreadTweaker {
         public static final int mainPriority = 1;
     }
 
-    public static @NotNull ExecutorService replaceForkJoinWorker(String name, int priority, int counter) {
+    public static @NotNull
+    //#if MC > 12101
+    //$$ TracingExecutor
+    //#else
+    ExecutorService
+    //#endif
+    replaceForkJoinWorker(String name, int priority, int counter) {
         AtomicInteger atomicInteger = new AtomicInteger(1);
-        return new ForkJoinPool(Mth.clamp(counter, 1, 0x7fff), (forkJoinPool) -> {
+        @SuppressWarnings("unused")
+        ExecutorService executorService = new ForkJoinPool(Mth.clamp(counter, 1, 0x7fff), forkJoinPool -> {
             //#if MC > 11502
             String workerName = "Worker-" + name + "-" + atomicInteger.getAndIncrement();
             //#else
@@ -83,12 +94,25 @@ public class ThreadTweaker {
 
             UtilAccessor.magiclib$getLogger().error(String.format("Caught exception in thread %s", thread), throwable);
         }, true);
+        return
+                //#if MC > 12101
+                //$$ new TracingExecutor(executorService);
+                //#else
+                executorService;
+                //#endif
     }
 
-    public static @NotNull ExecutorService replaceThreadWorker(
+    public static @NotNull
+    //#if MC > 12101
+    //$$ TracingExecutor
+    //#else
+    ExecutorService
+    //#endif
+    replaceThreadWorker(
             String name, int priority, Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
         AtomicInteger atomicInteger = new AtomicInteger(1);
-        return Executors.newCachedThreadPool((runnable) -> {
+        @SuppressWarnings("unused")
+        ExecutorService executorService = Executors.newCachedThreadPool((runnable) -> {
             String workerName = name + "-" + atomicInteger.getAndIncrement();
             MagicLib.getLogger().debug("Initialized " + workerName);
             Thread thread = new Thread(runnable);
@@ -98,6 +122,12 @@ public class ThreadTweaker {
             thread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
             return thread;
         });
+        return
+                //#if MC > 12101
+                //$$ new TracingExecutor(executorService);
+                //#else
+                 executorService;
+                //#endif
     }
 
     public static int getBootstrapCount() {

magiclib-better-dev/src/main/java/top/hendrixshen/magiclib/mixin/dev/threadtweak/UtilMixin.java
@@ -37,7 +37,12 @@ import top.hendrixshen.magiclib.impl.dev.MixinPredicates;
 import top.hendrixshen.magiclib.impl.dev.threadtweak.ThreadTweaker;
 
 import java.util.concurrent.Executor;
+
+//#if MC > 12101
+//$$ import net.minecraft.TracingExecutor;
+//#else
 import java.util.concurrent.ExecutorService;
+//#endif
 
 /**
  * Reference to <a href="https://github.com/UltimateBoomer/mc-smoothboot/blob/9a519ade89af24aa8b337dfed7d8eb8c0b62ec81/src/main/java/io/github/ultimateboomer/smoothboot/mixin/UtilMixin.java">SmoothBoot<a/>
@@ -65,7 +70,11 @@ public class UtilMixin {
     //$$ @Mutable
     //$$ @Shadow
     //$$ @Final
+    //#if MC > 12101
+    //$$ private static TracingExecutor DOWNLOAD_POOL;
+    //#else
     //$$ private static ExecutorService DOWNLOAD_POOL;
+    //#endif
     //#endif
     //#if MC < 11904
     @Mutable
@@ -76,12 +85,20 @@ public class UtilMixin {
     @Mutable
     @Shadow
     @Final
+    //#if MC > 12101
+    //$$ private static TracingExecutor IO_POOL;
+    //#else
     private static ExecutorService IO_POOL;
+    //#endif
     //#endif
     @Mutable
     @Shadow
     @Final
+    //#if MC > 12101
+    //$$ private static TracingExecutor BACKGROUND_EXECUTOR;
+    //#else
     private static ExecutorService BACKGROUND_EXECUTOR;
+    //#endif
 
     //#if MC > 11502
     @Shadow

magiclib-better-dev/versions/1.14.4-fabric/build.gradle
@@ -73,9 +73,9 @@ def apiDependencies = [
 
 // Module, Property prefix, Resolve condition, Transitive dependencies.
 def runtimeDependencies = [
-        ["maven.modrinth:in-game-account-switcher", "inGameAccountSwitcher", fabricLike, false],
-        ["maven.modrinth:imblocker"               , "imblocker"            , fabricLike, false],
-        ["maven.modrinth:imblocker-original"      , "imblocker"            , forgeLike , false],
+        ["maven.modrinth:in-game-account-switcher", "inGameAccountSwitcher", mcVersion < 12101, false],
+        ["maven.modrinth:imblocker"               , "imblocker"            , fabricLike       , false],
+        ["maven.modrinth:imblocker-original"      , "imblocker"            , forgeLike        , false],
 ]
 
 dependencies {

magiclib-better-dev/versions/1.21.3-fabric/gradle.properties
@@ -1,0 +1,19 @@
+# Dependency Versions
+dependencies.minecraft_dependency=>=1.21.2- <1.21.4-
+dependencies.minecraft_version=1.21.2
+
+# Fabric API 0.106.1+1.21.3
+dependencies.api.fabric_version=0.106.1+1.21.3
+# Mod Menu 12.0.0-beta.1
+# modmenu-12.0.0-beta.1.jar
+dependencies.api.modmenu_version=12.0.0-beta.1
+
+# IMBlockerFabric 1.0.24
+# imblockerfabric-1.0.24.jar
+dependencies.runtime.imblocker_version=1.0.24
+# In-Game Account Switcher 9.0.1
+# IAS-Fabric-1.21-9.0.1.jar
+dependencies.runtime.inGameAccountSwitcher_version=RQG6VufY
+
+# Publish properties
+publish.game_version=1.21.2\n1.12.3

magiclib-better-dev/versions/1.21.3-fabric/src/main/resources/magiclib-better-dev.accesswidener
@@ -1,0 +1,1 @@
+accessWidener	v2	named

magiclib-better-dev/versions/1.21.3-neoforge/gradle.properties
@@ -1,0 +1,17 @@
+# Dependency Versions
+dependencies.neoforge_version=21.3.0-beta
+dependencies.minecraft_dependency=1.21.3
+dependencies.minecraft_version=1.21.3
+
+# IMBlocker 4.0.9b
+# IMBlocker_4.0.9b+1.21.jar
+dependencies.runtime.imblocker_version=4.0.9b+1.21
+# In-Game Account Switcher 9.0.1
+# IAS-NeoForge-1.21-9.0.1.jar
+dependencies.runtime.inGameAccountSwitcher_version=erRoWz06
+
+# Loom Properties
+loom.platform=neoforge
+
+# Publish properties
+publish.game_version=1.21.3

commit 4b51dd1cf23efc6e266be4639ef7c8e68030f942

Fix TextRenderer's rendering issue accidentally introduced in #116
Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

magiclib-minecraft-api/src/main/java/top/hendrixshen/magiclib/impl/render/TextRenderer.java
@@ -158,7 +158,7 @@ public class TextRenderer {
                 this.verticalAlignment.getTranslateY(totalTextHeight), 0);
         context.translate(this.shiftX, this.shiftY, 0);
         //#if MC > 11605
-        //#if MC < 12101
+        //#if MC < 12102
         //$$ RenderSystem.applyModelViewMatrix();
         //#endif
         //#else
@@ -226,7 +226,7 @@ public class TextRenderer {
         RenderGlobal.enableDepthTest();
         //#endif
         positionTransformer.restore();
-        //#if MC > 11605 && MC < 12101
+        //#if MC > 11605 && MC < 12102
         //$$ RenderSystem.applyModelViewMatrix();
         //#endif
     }