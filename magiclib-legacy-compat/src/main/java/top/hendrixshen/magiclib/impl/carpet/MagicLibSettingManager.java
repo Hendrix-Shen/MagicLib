4c485549a6fa35a6d097556793d32cdf2bb27c86
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-01-04 20:03:13 +0800

    Migrating initialisation parameters to constructors
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

--- magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/impl/carpet/MagicLibSettingManager.java
+++ magiclib-legacy-compat/src/main/java/top/hendrixshen/magiclib/impl/carpet/MagicLibSettingManager.java
@@ -1,9 +1,10 @@
 package top.hendrixshen.magiclib.impl.carpet;
 
+import top.hendrixshen.magiclib.SharedConstants;
 import top.hendrixshen.magiclib.carpet.impl.WrappedSettingManager;
 
 public class MagicLibSettingManager extends WrappedSettingManager {
-    public MagicLibSettingManager(String version, String identifier, String fancyName) {
-        super(version, identifier, fancyName);
+    public MagicLibSettingManager() {
+        super(SharedConstants.getMagiclibVersion(), SharedConstants.getMagiclibIdentifier(), SharedConstants.getMagiclibName());
     }
 }
