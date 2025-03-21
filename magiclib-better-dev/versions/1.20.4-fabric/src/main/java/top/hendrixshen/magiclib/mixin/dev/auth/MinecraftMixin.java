2d01b9a5e7af473e1068d05d337ae9d9ee435735
Hendrix-Shen <HendrixShen@hendrixshen.top>
2025-03-21 11:42:13 +0800

    magiclib-better-dev 1.21.5-rc1 (fabric)
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

---
+++ magiclib-better-dev/versions/1.20.4-fabric/src/main/java/top/hendrixshen/magiclib/mixin/dev/auth/MinecraftMixin.java
@@ -0,0 +1,9 @@
+package top.hendrixshen.magiclib.mixin.dev.auth;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import top.hendrixshen.magiclib.api.preprocess.DummyClass;
+
+@Mixin(DummyClass.class)
+public class MinecraftMixin {
+}
