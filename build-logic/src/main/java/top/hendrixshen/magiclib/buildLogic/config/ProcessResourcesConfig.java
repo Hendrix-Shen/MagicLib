bf677ef52ea4d6c870b40ef1eabdd9045ec5173b
にゃんちゃん <HendrixShen@hendrixshen.top>
2025-08-08 21:14:33 +0800

    Update dependency gradle to v9 (#218)
    * Update plugin com.gradleup.shadow to v9.0.0-rc3
    
    * Gradle deprecation fix
    
    * Update dependency com.replaymod.preprocess to d452ef7 (fallen's fork)
    
    * Update dependency gradle to v9.0.0
    
    * Migrant ProcessResources configuration to build-logic
    
    * Add welcome message
    
    * Migrant to versionCatalogs and build-logic (magiclib-core)
    
    * Migrant non-minecraft dependencies to versionCatalogs
    
    * Add -Dfile.encoding=UTF-8 to gradle jvmargs
    
    * Create wrapper project dir before building
    
    * Fix buildscripts
    
    * Update build.yml
    
    * Fix build logic
    
    * Update plugin com.gradleup.shadow to v9.0.0
    
    * Update dependency checkstyle to v11
    
    ---------
    
    Signed-off-by: Hendrix-Shen <HendrixShen@hendrixshen.top>

---
+++ build-logic/src/main/java/top/hendrixshen/magiclib/buildLogic/config/ProcessResourcesConfig.java
@@ -0,0 +1,81 @@
+package top.hendrixshen.magiclib.buildLogic.config;
+
+import groovy.lang.Closure;
+import groovy.lang.GString;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.tasks.AbstractCopyTask;
+import org.gradle.language.jvm.tasks.ProcessResources;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcessResourcesConfig implements Plugin<Project> {
+    @Override
+    public void apply(@NotNull Project project) {
+        project.afterEvaluate(p -> {
+            p.getTasks().withType(ProcessResources.class).configureEach(task -> {
+                if (!"processResources".equals(task.getName())) {
+                    return;
+                }
+
+                this.addInputProperty(p, task);
+                this.filesMatching(p, task);
+                this.addExtraFile(p, task);
+            });
+        });
+    }
+
+    public void addInputProperty(Project project, ProcessResources task) {
+        Project parent = project.getParent();
+        assert parent != null;
+        Project root = project.getRootProject();
+        Map<String, Object> map = new HashMap<>();
+        map.put("mod_alias", parent.property("mod.id"));
+        map.put("mod_description", parent.property("mod.description"));
+        map.put("mod_homepage", parent.property("mod.homepage"));
+        map.put("mod_id", parent.property("mod.id").toString().replace("-", "_"));
+        map.put("mod_license", parent.property("mod.license"));
+        map.put("mod_name", parent.property("mod.name"));
+        map.put("mod_sources", parent.property("mod.sources"));
+        @SuppressWarnings("unchecked")
+        Closure<GString> getVersionWithCommitHash = (Closure<GString>) project.getRootProject().getExtensions().getExtraProperties().getProperties().get("getVersionWithCommitHash");
+        map.put("mod_version", getVersionWithCommitHash.call(project.getParent()));
+        map.put("root_mod_id", root.property("mod.id"));
+        map.put("root_mod_name", root.property("mod.name"));
+        map.put("root_mod_description", root.property("mod.description"));
+        task.getInputs().properties(map);
+    }
+
+    public void filesMatching(Project project, ProcessResources task) {
+        switch (project.getName()) {
+            case "fabric":
+                this.filesMatching(project, task, "fabric.mod.json");
+                break;
+            case "forge":
+                this.filesMatching(project, task, "META-INF/mods.toml");
+                break;
+            case "neoforge":
+                this.filesMatching(project, task, "META-INF/neoforge.mods.toml");
+                break;
+        }
+    }
+
+    public void filesMatching(Project project, ProcessResources task, String pattern) {
+        task.filesMatching(pattern, details -> details.expand(task.getInputs().getProperties()));
+    }
+
+    public void addExtraFile(Project project, ProcessResources task) {
+        Path rootDir = project.getRootDir().toPath();
+        Project parent = project.getParent();
+        assert parent != null;
+        task.from(rootDir.resolve("LICENSE"));
+        AbstractCopyTask copyTask = task.from(rootDir.resolve("icon.png"));
+
+        if ("fabric".equals(project.getName())) {
+            copyTask.into("assets/" + parent.property("mod.id"));
+        }
+    }
+}
---
+++ build-logic/src/main/java/top/hendrixshen/magiclib/buildLogic/config/ProcessResourcesMCConfig.java
@@ -0,0 +1,76 @@
+package top.hendrixshen.magiclib.buildLogic.config;
+
+import org.gradle.api.Project;
+import org.gradle.api.tasks.AbstractCopyTask;
+import org.gradle.language.jvm.tasks.ProcessResources;
+import org.jetbrains.annotations.NotNull;
+
+import net.fabricmc.loom.api.LoomGradleExtensionAPI;
+import net.fabricmc.loom.util.ModPlatform;
+import top.hendrixshen.magiclib.buildLogic.Util;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProcessResourcesMCConfig extends ProcessResourcesConfig {
+    private static final Map<String, List<ModPlatform>> FILE_PATTERNS = Util.make(() -> {
+        Map<String, List<ModPlatform>> res = new HashMap<>();
+        List<ModPlatform> list = new ArrayList<>();
+        list.add(ModPlatform.FABRIC);
+        res.put("fabric.mod.json", list);
+        list = new ArrayList<>();
+        list.add(ModPlatform.FORGE);
+        list.add(ModPlatform.NEOFORGE);
+        res.put("META-INF", list);
+        res.put("META-INF/mods.toml", list);
+        list = new ArrayList<>();
+        list.add(ModPlatform.NEOFORGE);
+        res.put("META-INF/neoforge.mods.toml", list);
+        return res;
+    });
+
+    private ModPlatform modPlatform;
+
+    @Override
+    public void apply(@NotNull Project project) {
+        // We need to wait for the configuration to evaluate before we can release magic
+        // in order to inject subproject's own input configuration.
+        project.afterEvaluate(super::apply);
+        this.modPlatform = project.getExtensions().getByType(LoomGradleExtensionAPI.class).getPlatform().get();
+    }
+
+    @Override
+    public void addInputProperty(Project project, ProcessResources task) {
+        super.addInputProperty(project, task);
+        Map<String, Object> map = new HashMap<>();
+        map.put("minecraft_dependency", project.property("dependencies.minecraft_dependency"));
+        task.getInputs().properties(map);
+    }
+
+    @Override
+    public void filesMatching(Project project, ProcessResources task) {
+        ProcessResourcesMCConfig.FILE_PATTERNS.forEach((pattern, platforms) -> {
+            if (platforms.stream().anyMatch(platform -> platform == this.modPlatform)) {
+                this.filesMatching(project, task, pattern);
+            } else {
+                task.exclude(pattern);
+            }
+        });
+    }
+
+    @Override
+    public void addExtraFile(Project project, ProcessResources task) {
+        Path rootDir = project.getRootDir().toPath();
+        Project parent = project.getParent();
+        assert parent != null;
+        task.from(rootDir.resolve("LICENSE"));
+        AbstractCopyTask copyTask = task.from(rootDir.resolve("icon.png"));
+
+        if (this.modPlatform == ModPlatform.FABRIC) {
+            copyTask.into("assets/" + parent.property("mod.id"));
+        }
+    }
+}
